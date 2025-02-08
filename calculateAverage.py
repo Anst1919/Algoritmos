b7d9530a0502eff7b5027b4cc6cd54640862fa71
Ashly Naomi <naosanchezt@gmail.com>
2025-02-08 12:59:25 -0600

    Ejercicio en un lenguaje de programación (Python)
    Exporté un trabajo de PSeInt a un lenguaje de programación, el que elegí fue Python

--- calculateAverage.py
+++ calculateAverage.py
@@ -1,8 +1,3 @@
-# Este codigo ha sido generado por el modulo psexport 20230904-w32 de PSeInt.
-# Es posible que el codigo generado no sea completamente correcto. Si encuentra
-# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
-
-
 if __name__ == '__main__':
 	# Ask the user for the 5 grades
 	print("Enter the 5 grades")
@@ -15,3 +10,4 @@ if __name__ == '__main__':
 	average = (grade1+grade2+grade3+grade4+grade5)/5
 	# Display the average result
 	print("The average of the 5 grades is: ",average)
+	
