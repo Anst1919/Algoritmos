0b3c12f5f32b4c5cf2c97e5a167ccf56364341b9
Ashly Naomi <naosanchezt@gmail.com>
2025-02-27 20:01:23 -0600

    3 Ejercicio de tarea - Parcial 2
    Realicé nuestro primer ejercicio en CodeSandbox con el lenguaje de programación Python, hicimos concatenaciones y operaciones matemáticas básicas, así como pedirle al usuario dos números para así realizar las operaciones indicadas.

---
+++ main.py
@@ -0,0 +1,52 @@
+print("Hello CodeSandbox!")
+print("Valeria hermosa")
+print("Sebastián impuntual")
+print("Yahir inteligente")
+print("Miguel burro")
+print("Dylan")
+print("Marlin")
+print("Levi")
+print("Jovany")
+print("Cassandra")
+print("Biskui")
+
+""" Vamos a concatenar  """
+nombre = " Ashly "
+apellido = " Sanchez "
+print("Hola mi nombre es: ",nombre) #Hola mi nombre es: Ashly
+print("Hola mi nombre es: ",nombre, apellido) #Hola mi nombre es: Ashly Sanchez
+print("Hola mi nombre es: ",nombre,"y mi apellido es:", apellido) #Hola mi nombre es: Ashly y mi apellido es: Sanchez
+
+""" Concatenación con diferentes tipos de datos  """
+mi_edad=19
+mi_nombre="Ashly"
+mi_apellido_paterno="Sanchez"
+mi_apellido_materno="Torres"
+soy_estudiante= True
+soy_maestro= False
+
+print("Hola, mi nombre es",mi_nombre,"y mi apellido paterno es",mi_apellido_paterno,"y mi apellido meterno es",mi_apellido_materno,"mi edad es",mi_edad,"y soy estudiante de universidad." )
+
+""" Uso de type - Averigua que tipo de dato recibe  """
+print(type(mi_edad)) #<class 'int'>
+print(type(mi_nombre)) #<class 'str'>
+print(type(soy_estudiante)) #<class 'bool'>
+
+""" operaciones matemáticas """
+numero_uno=10
+numero_dos=5
+print(numero_uno+numero_dos) #15
+print(numero_uno-numero_dos) #5
+print(numero_uno*numero_dos) #50
+print(numero_uno/numero_dos) #2.0
+
+""" Mathematical operations """
+#Ask the user to enter the numbers
+number_one = int(input("Enter the first number: "))
+number_two = int(input("Enter the second number: "))
+
+#Perform the operation and show the result
+print(number_one + number_two) # Print the result of adding the two numbers
+print(number_one - number_two) # Print the result of subtracting the second number from the first
+print(number_one * number_two) # Print the result of multiplying the two numbers
+print(number_one / number_two) # Print the result of dividing the first number by the second
