39fb9b75125f8212a51aeff9761ec4bbd5b23fd0
Ashly Naomi <naosanchezt@gmail.com>
2025-03-03 11:13:42 -0600

    9 Ejercicio en clase - Parcial 2
    Asignación en clase.

---
+++ ejercicio1.py
@@ -0,0 +1,29 @@
+#Número se asigna un valor a una variable
+numero = 10
+
+#Se imprime el valor de la variable número, ej 10
+print("El número asignado a la variable número  es: ", numero)
+
+""" 
+Actividad de clase: definir valores de variables y las van a imprimir; sobre algun hobby usarás:
+-enteros
+-booleanos
+-caracteres
+Tiempo: 8min 
+"""
+
+# Definición de variables
+# Entero: Número de meses aprendiendo a cocinar
+meses_aprendiendo = 2 
+
+# Booleano: Indica si cocinar es su hobby favorito
+es_hobby_favorito = True  
+
+# Caracter: Nivel de dificultad que percibe (A: Alta, M: Media, B: Baja)
+nivel_dificultad = 'M'  
+
+# Impresión de valores
+print("Tiempo aprendiendo a cocinar:", meses_aprendiendo, "meses")
+print("¿Es cocinar mi hobby favorito?", es_hobby_favorito)
+print("Nivel de dificultad percibido:", nivel_dificultad)
+
